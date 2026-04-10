b03f6b2d9bea31110e8f3348d1c4f064f40dcac8
jose6668 <jlvargas-2022b@corhuila.edu.co>
2026-04-10 11:17:58 -0500

    Trigger step 2 validation

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import * as core from "@actions/core";
 import getJoke from "./joke.js";
 
 async function run() {
+  // Surface the fetched joke as an action output for later workflow steps.
   const joke = await getJoke();
   console.log(joke);
   core.setOutput("joke", joke);
